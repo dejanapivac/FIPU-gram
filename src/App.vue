75ae00ba5ab78023770c0f408c4cf61ccb16705d
Dejana Pivac <dpivac@student.unipu.hr>
2020-12-23 15:46:27 +0100

    Frist commit

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,69 @@
 <template>
-    <div id="app">
-        <div id="nav">
-            <router-link to="/">Home</router-link>
-            |
-            <router-link to="/login">Login</router-link>
-            |
-            <router-link to="/signup">Sign up</router-link>
-        </div>
-
-        <div class="container">
-            <router-view />
-            <!-- tu router dinamicki ubaci screen -->
-        </div>
-    </div>
+  <div id="app">
+    <nav id="nav" class="navbar navbar-expand-lg navbar-light">
+          <a class="navbar-brand" href="#">
+             <img 
+                 src="@/assets/logooo.jpg"
+                 height="50" 
+                 class="d-inline-block align-top" 
+                 alt="" 
+                loading="lazy"
+              />    
+          </a>
+          <button 
+              class="navbar-toggler" 
+              type="button" 
+              data-toggle="collapse" 
+              data-target="#navbarToggler" 
+              aria-controls="navbarToggler" 
+              aria-expanded="false" 
+              aria-label="Toggle navigation"
+          >
+           <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarToggler">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+              <li class="nav-item">
+               <router-link to="/" class="nav-link">Home</router-link>
+              </li>
+              <li class="nav-item">
+               <router-link to="/login" class="nav-link">Login</router-link> 
+              </li>
+              <li class="nav-item">
+               <router-link to="/signup" class="nav-link">Sign up</router-link>
+              </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" placeholder="Pretraga" aria-label="Search">
+              
+            </form>
+          </div>
+          
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
 <style lang="scss">
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
 #nav {
-    padding: 30px;
+  padding: 30px;
+  //background-color: white !important; gore je ispravnije ako maknem bg-light
+  
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-    a {
-        font-weight: bold;
-        color: #2c3e50;
-
-        &.router-link-exact-active {
-            color: #42b983;
-        }
+    &.router-link-exact-active {
+      color: green;;
     }
+  }
 }
 </style>
